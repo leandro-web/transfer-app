7153f5ef080bce10d27cccf7a4402e4291197103
leandro-web <leandro-web@hotmail.com>
2022-10-04 10:38:29 -0300

    Create model and migrations

--- database/migrations/2022_10_04_122338_create_profiles_table.php
+++ database/migrations/2022_10_04_122338_create_profiles_table.php
@@ -13,15 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('profiles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('doc')->unique();
-            $table->bigInteger('profile_id');
-            $table->rememberToken();
+            $table->string('profile');
             $table->timestamps();
         });
     }
@@ -33,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('profiles');
     }
 };
--- database/migrations/2022_10_04_125752_create_balances_table.php
+++ database/migrations/2022_10_04_125752_create_balances_table.php
@@ -13,15 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('balances', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('doc')->unique();
-            $table->bigInteger('profile_id');
-            $table->rememberToken();
+            $table->bigInteger('user_id');
+            $table->float('balance');
             $table->timestamps();
         });
     }
@@ -33,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('balances');
     }
 };
